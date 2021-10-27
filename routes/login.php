5a6cda9ac65c0fbeddcd30d34d94a5fdcec9cdd1
Valentin <valentin.d77@free.fr>
2021-10-27 13:11:51 +0200

    api logout

--- routes/login.php
+++ routes/login.php
@@ -3,3 +3,5 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->post('/login', ['uses' => 'LoginController@login']);
+
+$router->get('/logout', ['uses' => 'LoginController@logout', 'middleware' => 'auth']);
