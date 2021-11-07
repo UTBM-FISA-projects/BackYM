9685985e555f17c85cab1c90a88cea169b82fcfe
Valentin <valentin.d77@free.fr>
2021-11-07 21:59:31 +0100

    api proposition de missions

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -2,10 +2,24 @@
 
 namespace App\Policies;
 
+use App\Models\Notification;
+use App\Models\NotificationType;
 use App\Models\Task;
+use App\Models\User;
 
 class TaskPolicy
 {
+    public function accept(User $user, Task $task): bool
+    {
+        $notif = Notification::query()
+            ->where('parameters->task', $task->id_task)
+            ->where('recipient', $user->id_user)
+            ->where('id_notification_type', NotificationType::$TASK_PROPOSAL)
+            ->first();
+
+        return $notif !== null and $task->id_executor === null;
+    }
+
     public function update($user, Task $task): bool
     {
         return $user->id_user === $task->id_executor
--- routes/task.php
+++ routes/task.php
@@ -3,6 +3,8 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->put('/tasks/{id:[1-9]\d*}/accept', ['uses' => 'TaskController@accept']);
+
     $router->put('/tasks/{id:[1-9]\d*}', ['uses' => 'TaskController@update']);
 
     $router->post('/tasks', ['uses' => 'TaskController@create']);
